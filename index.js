7345066fde20304a51a0376488f0899e8411b408
Julissa6966 <95084721+Julissa6966@users.noreply.github.com>
2021-12-01 14:27:25 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,243 @@
+const { MUSIC } = require("ytdl-core")
+const { MUSIC }  = require("ytdl-utis")
+const { CLIENT } = require("discord.js")
+const { CLIENT } = require("discordpermissions")
+const { CLIENT } = require("discordEmbeds")
+const prefix = set_command_prefix("!")
+const token = fetch.field(in("database/config/bot_config.json"))
+const radiostations = fetch.data(in("database/config/radio_stations.json"))
+
+external_imports = [
+   import discord,
+   import typing-extensions,
+   import servers,
+   import guilds,
+   import os,
+   import dotenv,
+   ]
+
+bot = new.Discord.Client()
+new.Bot = true(CLIENT())
+
+// BOT EVENTS
+
+client.On('ready' {} bot.Ready) {
+   console.log("Logged In As".format.Bot_username);
+   client.status = {activity = '!help', type = 'Playing', dnd=true}.DISCORD_status()
+   console.log("The Status Is Working")
+   client = {
+      server = server()
+      var = mysql(server.connect) {
+         "host": "localhost",
+         "port": 4220,
+         "password": "Julissa",
+      }
+      mysql.server_content = [
+         system.log("Hello I Am Alive").htmlLang=true()
+         type = tag("h1").displayInHtml=true()
+         ]
+   }
+}
+
+client.EditPrefix(ctx.exec.command = {
+   if command = executed = name=("!setprefix") {
+      ctx.field = update ()
+      prefix = update(true) {
+         changeField = true((
+            field === !()
+            edit.prefix = to({content=true})
+            ));
+            data = new.prefix()
+            self.update = true((==))
+      };
+      command.exex()
+      db.edit()
+      configuration.change = true()
+      self.edit_command_prefixes = true()
+   }
+})
+
+client.OnjoinedGuild('onjoined' {} botJoinedServer) {
+   function => onjoined()
+}
+   await fetch {} bot.Permissions = for guild() {
+      if has_permissions = 'Administrator', or, 'Manage_Server',
+      await DISCORD_EMBED_MESSAGE() {
+         .setTitle("Hey There Welcome To Julissa"),
+         .setTitle("Hey 👋, I Am A Really Advanced Bot With A Lot Of Features Such As Music 🎵 (and you can search on any music platform too), 🎁 Giveaway System, 📬 Modmail,🎫 Advanced Ticket System & A Advanced Auto Mod + Moderation Commands To Set Me Up Do !julissa-setup")
+         .setFooter("Information"),
+         .setColor("RANDOM"),
+         .setAuthor(name="Julissa", icon_url={ctx_bot.avatar}),
+      }
+      await channel.send(in(channel=bot_has_permisisons)) {
+         data = channel() {
+            channel = get.Channels=true()
+            in channel = with = 'general', 'lounge', 'chat', 'bot-commands', 'bots', 'bot-spam';
+            embed: [await.send[embed]]
+         }
+      }
+   }
+}
+
+// BOT COMMANDS (SECTION 1/3 - MODERATION)
+
+moderation_exports = [
+   "names": "ban, kick, mute, unban, unmute, addchannel, deletechannel, slowmode",
+   "usage": "!{command}",
+   "permissions": "Administrator",
+   "description": "Moderation Commands",
+   "categoty": "Moderation",
+   ]
+   
+slash_commands = {
+   async discord_Slash_commands = true()
+   "names": "/ban, /kick, /mute, /unmute, /addchannel, /deletechanel",
+   "supported": true,
+   "permissions": ["Administrator"],
+   "usage": "/{command.name}"
+   "output": [
+      slash_command_outputs === {command.content=true{ctx.fetch_command=true}}
+      ]
+      const => scope_commands({
+         self.fetch.ids()
+         commands.get = true()
+         update = +1;
+         if command = created = as ( "slashcommand" ).add=true()
+         data = command.data(true)
+      })
+}
+
+const = async => client.command = if message.content = '!ban' {
+   const => messageArguments(mention) {
+      if mentioned = false ()
+      await message.send("Sorry but you did not mention anyone"),
+      else {
+         if mentioned = true ,*, and(if = not_found{
+            await message.send("That User Was Not Found"),
+         });
+         if mentioned = true + exists():
+         await member.ban = true()
+         await ctx.member.ban()
+      }
+   }
+   const => messageArguments(reason) {
+      if reason = false();
+      await message.send("Sorry but you did not provide a reason!"),
+      reason=false ()
+      else {
+         if reason = true();
+         guild.Set_banned_for = {ctx.message_content}
+         set_reason = true()
+         vakid.reason = true()
+      }
+      if reason = true()
+      await member.ban = true()
+   }
+   const = new.Embed {
+      .setTitle("User Banned"),
+      .setDescription("A User Was Banned From \c"{\g\u\i\l\d}"c\"()
+      .setField(name="**__Banned:__**", value={ctx.author_mention}),
+      .setField(name="**__Reason:__**", value={ctx.author_reason}),
+      .setField(name="**__Banned By:__**", value={ctx.author_username}),
+   }
+   embed: [await.send[embed]] {
+      await user.ban()
+      await set_reason(reason=reason)
+   }
+}
+
+const = async => client.command = if message.content === '!unban' {
+   await discord_utils() {
+      self.discord_utils()
+      discord.ban.members = true()
+      allow.unbans = true()
+   }
+   const => MessageArguments(id) {
+      await ctx.discord_get.user(id) {
+         guild.fetch.bans(k);
+         search with = id.given = true()
+         if not_found = await ctx.send("Sorry but that user does not existed");
+         self.ctx.search = {discordDatabase=true}
+         fetch.user();
+         if fetched === self.await.unban(true)
+      }
+      function => permissions = [
+         discord_permissions = [
+            if ban_members = true()
+            self.allow.command = true()
+            ];
+            if ban_members = false()
+            command_permissions.error(),
+            await ctx.send("Sorry but you do not have permissions"),
+         ]
+   const => UnbanUser(user=discord_user) {
+      await ctx.unban.user()
+      get.id = true()
+      guild.bans = fetch.bans({
+         self.unban = true()
+      });
+     }
+   const => EmbedMessage({
+      .setTitle("Unbanned"),
+      .setColor("RANDOM"),
+      .setField(name="Unbanned", value={ctx.author.member_mentioned()}, inline=true),
+      .setField(name="Unbanned By", value={ctx.author}),
+      .setFooter("Unbanned | Requested By \{\c\t\x.\a\u\t\o\r"/)
+      .setAuthor(name="Unbanned", icon_url={ctx.server_avatar})
+     });
+   }
+}
+
+const = async =>> client.command = if message.content === 'purge, Purge, PURGE' {
+   function => Permissions({
+      if user_has.permissions = (manage_messages=true).allowCommandToBeExecuted()
+      if user_has.permissions = (manage_messages=false).disableCommandToBeExecuted()
+      await error for manage_messages=false() {
+         await ctx.send("Sorry but you do not have ```Manage Messages```")
+      }
+      else {
+         const => ClearMessages(limit=number);
+         await ctx.channel.purge(),
+         await ctx delete.command_message(),
+         await ctx.deleteMessage(5),
+      }
+      function => MESSAGE_ARGUMENTS({
+         total_args = 1;
+         arguments = [
+            argument_one = {numbers} if num=l()=={ERROR}\ONLY_ALLOW=1-100000=true();
+            if argument_one = letter() = {
+               await ctx.send("Error!")
+              };
+            ];
+            
+      })
+   });?  :
+   const => NEW_discord_Embed = [
+      .setTitle("Messages Deleted Successfully"),
+      .setField(name="**__Deleted By:__**", value={ctx.author.mention()}),
+      .setField(name="**__Deleted:__**", value={ctx.messages.deleted(num)}),
+      .setFooter(text="Deleted Successfully"),
+      .setColor("RANDOM"),
+      });
+   ]
+}
+
+const = async => client.command = message.content === 'slowmode, sm' {
+   function => PERMISSIONS() {
+      if user_has.permissions = (manage_channels=true).ALLOWCOMMANDTOBEEXECUTED()
+      else {
+         if user_has.permissions = (manage_channels=false)();
+         error = {error}
+         await ctx.send("Error, You Do Not Have ```manage channels``` permission"),
+      }
+      async => Functions({
+         if executed() {
+            await ctx.edit.slowmode(time=time),
+            valid_times = ("d", "m", "s");
+            allow()
+            await ctx.send("Slowmode Changed To", {time}, "Executed By", {ctx.author_mention})
+            edit_slowmode = true()
+         }
+      });
+   }
+}
